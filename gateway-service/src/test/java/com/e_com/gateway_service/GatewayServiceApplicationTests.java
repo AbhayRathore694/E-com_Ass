dec82674456fd38e8504e4232ad519eab0e7bb19
harshkumar2003 <harshk8876@gmail.com>
2026-01-19 13:49:28 +0530

    first commit

---
+++ gateway-service/src/test/java/com/e_com/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_com.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
